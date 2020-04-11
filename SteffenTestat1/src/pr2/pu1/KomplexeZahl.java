947e2d305bcfc3c776a42b9f13b2b04a1362c456
steff <steff@192.168.24.232>
2020-04-11 11:43:31 +0200

    up

--- SteffenTestat1/src/pr2/pu1/KomplexeZahl.java
+++ SteffenTestat1/src/pr2/pu1/KomplexeZahl.java
@@ -2,47 +2,46 @@ package pr2.pu1;
 
 public class KomplexeZahl {
 
-	private final double realanteil;
-	private final double imaginaeranteil;
+	private final double re;
+	private final double im;
 
 	public double re() {
-		return this.realanteil;
+		return this.re;
 	}
 
 	public double im() {
-		return this.imaginaeranteil;
+		return this.im;
 	}
 
 	public KomplexeZahl(double realanteil, double imaginaeranteil) {
-		this.realanteil = realanteil;
-		this.imaginaeranteil = imaginaeranteil;
+		this.re = realanteil;
+		this.im = imaginaeranteil;
 	}
 
 	@Override
 	public String toString() {
-		return this.realanteil + " + " + this.imaginaeranteil + "i";
+		return this.re + " + " + this.im + "i";
 	}
 
-	public KomplexeZahl add(KomplexeZahl komplexeZahl) {
-		return new KomplexeZahl((this.realanteil + komplexeZahl.re()), (this.imaginaeranteil + komplexeZahl.im()));
+	public KomplexeZahl add(KomplexeZahl kZ) {
+		return new KomplexeZahl((this.re + kZ.re()), (this.im + kZ.im()));
 	}
 
-	public static KomplexeZahl add(KomplexeZahl komplexeZahl1, KomplexeZahl komplexeZahl2) {
-		return new KomplexeZahl((komplexeZahl1.re() + komplexeZahl2.re()), (komplexeZahl1.im() + komplexeZahl2.im()));
+	public static KomplexeZahl add(KomplexeZahl kZ1, KomplexeZahl kZ2) {
+		return new KomplexeZahl((kZ1.re() + kZ2.re()), (kZ1.im() + kZ2.im()));
 	}
 
-	public KomplexeZahl prod(KomplexeZahl komplexeZahl) {
-		return new KomplexeZahl((this.realanteil * komplexeZahl.re() - this.imaginaeranteil * komplexeZahl.im()),
-				(this.realanteil * komplexeZahl.im() + this.imaginaeranteil * komplexeZahl.re()));
+	public KomplexeZahl prod(KomplexeZahl kZ) {
+		return new KomplexeZahl((this.re * kZ.re() - this.im * kZ.im()), (this.re * kZ.im() + this.im * kZ.re()));
 	}
 
-	public static KomplexeZahl prod(KomplexeZahl komplexeZahl1, KomplexeZahl komplexeZahl2) {
-		return new KomplexeZahl((komplexeZahl1.re() * komplexeZahl2.re() - komplexeZahl1.im() * komplexeZahl2.im()),
-				(komplexeZahl1.re() * komplexeZahl2.im() + komplexeZahl1.im() * komplexeZahl2.re()));
+	public static KomplexeZahl prod(KomplexeZahl kZ1, KomplexeZahl kZ2) {
+		return new KomplexeZahl((kZ1.re() * kZ2.re() - kZ1.im() * kZ2.im()),
+				(kZ1.re() * kZ2.im() + kZ1.im() * kZ2.re()));
 	}
 
 	public static void main(String[] args) {
 		KomplexeZahl x = new KomplexeZahl(2.5, 4.5);
-		System.out.println("x = " + x.toString());
+		System.out.println("x = " + x);
 	}
 }
