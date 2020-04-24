3bb0fb300231d6bcc187a0cbdfc62f45b6c67584
steff <steff@192.168.24.232>
2020-04-24 18:55:42 +0200

    up

---
+++ SteffenTestat2/src/pr2/pu2/CopyTest.java
@@ -0,0 +1,14 @@
+package pr2.pu2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CopyTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
