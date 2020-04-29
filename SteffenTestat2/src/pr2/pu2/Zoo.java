8434437322d5a1cde6b8a354eb6641d33c1f3e51
Steffen <Steffen@192.168.24.208>
2020-04-29 13:44:58 +0200

    up

---
+++ SteffenTestat2/src/pr2/pu2/Zoo.java
@@ -0,0 +1,12 @@
+package pr2.pu2;
+
+import java.util.Iterator;
+
+public class Zoo<E> implements Iterable<E> {
+
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
