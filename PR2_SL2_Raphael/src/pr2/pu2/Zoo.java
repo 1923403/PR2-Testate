ca0c75338007d0a68024356f97e5d392f0200f89
Raphael <Raphael@DESKTOP-U3AJ5B0.fritz.box>
2020-04-30 13:34:35 +0200

    up

--- PR2_SL2_Raphael/src/pr2/pu2/Zoo.java
+++ PR2_SL2_Raphael/src/pr2/pu2/Zoo.java
@@ -2,31 +2,27 @@ package pr2.pu2;
 
 import java.util.Iterator;
 
-
 public class Zoo implements Iterable<Tier> {
-
-	
 
 	public static void main(String[] args) {
 		ArtenGehege<Hase> hasenstall = new ArtenGehege<>();
 
 		var hase1 = new Hase();
-		//Unterschied var zu Hase?
+		// Unterschied var zu Hase?
 		Hase hase2 = new Hase();
 		Hase hase3 = new Hase();
-		
 
 		hasenstall.einsperren(hase1);
 		hasenstall.einsperren(hase2);
-		//hasenstall.einsperren(hase3);
+		// hasenstall.einsperren(hase3);
 
 		System.out.println(hasenstall.size());
-		
-		 Iterator<Hase> it = hasenstall.iterator();
-		while(it.hasNext()) {
-		      Hase obj = it.next();
-		      System.out.println(obj.hp);
-		    }
+
+		Iterator<Hase> it = hasenstall.iterator();
+		while (it.hasNext()) {
+
+			System.out.println(it.next().hp);
+		}
 
 	}
 
