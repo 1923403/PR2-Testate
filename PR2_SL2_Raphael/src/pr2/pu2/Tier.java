860774e6ac196c7240fc8027cb766d643f8334b7
Raphael <Raphael@DESKTOP-U3AJ5B0.fritz.box>
2020-04-30 13:42:27 +0200

    up

--- PR2_SL2_Raphael/src/pr2/pu2/Tier.java
+++ PR2_SL2_Raphael/src/pr2/pu2/Tier.java
@@ -14,55 +14,45 @@ public class Tier extends Thread {
 	public void run() {
 		while (hp > 0) {
 			this.move();
-			hp -= 1;
+			this.hp -= 1;
 			try {
 				Thread.sleep(100);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			System.out.println(this.getName() + " has " + hp + " and is at x = " + x + " and y = " + y);
-
 		}
 	}
 
 	public static Tier create() {
 		Tier tier = new Tier((int) (Math.random() * 100));
-		counter += 1;
+		Tier.counter++;
 		tier.setName("Tier-" + String.format("%03d", Tier.counter));
 		tier.start();
 		return tier;
 	}
 
-	public void move() {
-		// Achten Sie bei der Umsetzung darauf, dass die Methode auch von anderen
-		// Threads
-		// aufgerufen werden kann.
-		synchronized (this) {
-			int d = (int) (Math.random() * 5);
-			if (d == 1) {
-				x += 1;
-			} else if (d == 2) {
-				x -= 1;
-			} else if (d == 3) {
-				y += 1;
-			} else if (d == 4) {
-				y -= 1;
-			} else {
-			}
+	public synchronized void move() {
+		switch ((int) (Math.random() * 5)) {
+		case 0:
+			break;
+		case 1:
+			this.x++;
+			break;
+		case 2:
+			this.x--;
+			break;
+		case 3:
+			this.y++;
+			break;
+		case 4:
+			this.y--;
+			break;
 		}
-
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// Tier a = new Tier(33);
-		// a.start();
-
-		// Tier.create();
-		Tier.create();
-		Hase.create();
-
+		Tier tier = new Tier(33);
+		tier.start();
 	}
-
 }
