42808184ebd0d40d4f462b891e7a41f3db8fa853
Raphael <Raphael@DESKTOP-U3AJ5B0.fritz.box>
2020-04-30 11:29:12 +0200

    up

--- PR2_SL2_Raphael/src/pr2/pu2/ArtenGehege.java
+++ PR2_SL2_Raphael/src/pr2/pu2/ArtenGehege.java
@@ -2,7 +2,7 @@ package pr2.pu2;
 
 import java.util.ArrayList;
 
-public class ArtenGehege<E> extends ArrayList<E> {
+public class ArtenGehege<E extends Tier> extends ArrayList<E> {
 
 	public void einsperren(E x) {
 		this.add(x);
@@ -12,10 +12,9 @@ public class ArtenGehege<E> extends ArrayList<E> {
 
 		ArtenGehege<Hase> hasenstall = new ArtenGehege<>();
 
-		Hase hase1 = new Hase();
+		var hase1 = new Hase();
 		
-		//Unterschied var zu Hase?
-		Hase hase2 = new Hase();
+		var hase2 = new Hase();
 		var hase3 = new Hase();
 
 		hasenstall.einsperren(hase1);
--- PR2_SL2_Raphael/src/pr2/pu2/Zoo.java
+++ PR2_SL2_Raphael/src/pr2/pu2/Zoo.java
@@ -2,34 +2,38 @@ package pr2.pu2;
 
 import java.util.Iterator;
 
-public class Zoo implements Iterable<ArtenGehege> {
 
-	@Override
-	public Iterator<ArtenGehege> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+public class Zoo implements Iterable<Tier> {
+
+	
 
 	public static void main(String[] args) {
 		ArtenGehege<Hase> hasenstall = new ArtenGehege<>();
 
-		Hase hase1 = new Hase();		
+		var hase1 = new Hase();
 		//Unterschied var zu Hase?
 		Hase hase2 = new Hase();
-		//var hase3 = new Hase();
-		Hase.create();
+		Hase hase3 = new Hase();
+		
 
 		hasenstall.einsperren(hase1);
 		hasenstall.einsperren(hase2);
 		//hasenstall.einsperren(hase3);
 
 		System.out.println(hasenstall.size());
+		
 		 Iterator<Hase> it = hasenstall.iterator();
 		while(it.hasNext()) {
 		      Hase obj = it.next();
-		      System.out.println(obj.getName());
+		      System.out.println(obj.hp);
 		    }
 
+	}
+
+	@Override
+	public Iterator<Tier> iterator() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
