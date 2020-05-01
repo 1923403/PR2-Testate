8530df071429f1c9fc03855951215c2d4947a3b6
Steffen <Steffen@Steffen-Desktop.lan>
2020-05-01 11:50:39 +0200

    up

--- SteffenTestat2/src/pr2/pu2/MyComparator.java
+++ SteffenTestat2/src/pr2/pu2/MyComparator.java
@@ -5,11 +5,6 @@ import java.util.Comparator;
 public class MyComparator<E extends Tier> implements Comparator<E> {
 
 	public int compare(E tier1, E tier2) {
-		if (tier1.health > tier2.health)
-			return -1;
-		else if (tier1.health == tier2.health)
-			return 0;
-		else
-			return 1;
+		return tier2.health - tier1.health;
 	}
 }
