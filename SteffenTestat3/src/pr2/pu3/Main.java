6de8b04a79edcdd1383c4db7f7b173f93b18b203
steff <steff@192.168.24.232>
2020-05-07 18:11:12 +0200

    up

---
+++ SteffenTestat3/src/pr2/pu3/Main.java
@@ -0,0 +1,16 @@
+package pr2.pu3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// AUFGABE 1
+		var intList = new ListNode<>(1,
+				new ListNode<>(1, new ListNode<>(2, new ListNode<>(3, new ListNode<>(5, new ListNode<>(8))))));
+
+		var stringList = new ListNode<>("Hallo", new ListNode<>("Welt",
+				new ListNode<>("das", new ListNode<>("Wetter", new ListNode<>("ist", new ListNode<>("schön"))))));
+
+		System.out.println(intList.toListString());
+		System.out.println(stringList.toListString());
+	}
+}
