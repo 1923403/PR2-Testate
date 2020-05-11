6ac8a193c0f31e044dfc0e51b4c981893948b5ad
Steffen <Steffen@192.168.24.208>
2020-05-11 16:19:35 +0200

    up

--- SteffenTestat3/src/pr2/pu3/Main.java
+++ SteffenTestat3/src/pr2/pu3/Main.java
@@ -12,17 +12,31 @@ public class Main {
 //
 //		System.out.println(intList.toListString());
 //		System.out.println(stringList.toListString());
-		
+
 		// AUFGABE 5
-		var treeNode1 = new TreeNode<>(null);
-		var treeNode2 = new TreeNode<>(5, new TreeNode<>(1), null);
-		var treeNode3 = new TreeNode<>(5, new TreeNode<>(1), new TreeNode<>(7));
-		var treeNode4 = new TreeNode<>(5, new TreeNode<>(1), new TreeNode<>(6));
-		var treeNode5 = new TreeNode<>(5, new TreeNode<>(7), new TreeNode<>(1));
-		var treeNode6 = new TreeNode<>(5, new TreeNode<>(7), new TreeNode<>(1));
-		var treeNode7 = new TreeNode<>(6, new TreeNode<>(7), new TreeNode<>(1));
-		
-		System.out.println(treeNode5.equalStructure(treeNode6));
-		System.out.println(treeNode6.equalStructure(treeNode7));
+//		var treeNode1 = new TreeNode<>(null);
+//		var treeNode2 = new TreeNode<>(5, new TreeNode<>(1), null);
+//		var treeNode3 = new TreeNode<>(5, new TreeNode<>(1), new TreeNode<>(7));
+//		var treeNode4 = new TreeNode<>(5, new TreeNode<>(1), new TreeNode<>(6));
+//		var treeNode5 = new TreeNode<>(5, new TreeNode<>(7), new TreeNode<>(1));
+//		var treeNode6 = new TreeNode<>(5, new TreeNode<>(7), new TreeNode<>(1));
+//		var treeNode7 = new TreeNode<>(6, new TreeNode<>(7), new TreeNode<>(1));
+//		
+//		System.out.println(treeNode5.equalStructure(treeNode6));
+//		System.out.println(treeNode6.equalStructure(treeNode7));
+
+		var intTreeA = new TreeNode<Integer>(0, new TreeNode<>(3, new TreeNode<>(38), new TreeNode<>(54)),
+				new TreeNode<>(5));
+		var intTreeB = new TreeNode<Integer>(0, new TreeNode<>(3, new TreeNode<>(38), new TreeNode<>(55)),
+				new TreeNode<>(5));
+		var intTreeC = new TreeNode<Integer>(null);
+		var intTreeD = new TreeNode<Integer>(null);
+
+//		intTreeA.printInorder();
+//		intTreeB.printInorder();
+//		intTreeA=null;
+//		intTreeB=null;
+		System.out.println(intTreeA.equalStructure(intTreeB));
+		System.out.println(intTreeC.equalStructure(intTreeD));
 	}
 }
