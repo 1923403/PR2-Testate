e708f6aa966ad8f3c71081c5fcc74595027c03b7
steff <steff@192.168.24.232>
2020-05-11 17:26:20 +0200

    up

---
+++ SteffenTestat3/src/pr2/pu3/SearchTreeNodeTest.java
@@ -0,0 +1,14 @@
+package pr2.pu3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearchTreeNodeTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
