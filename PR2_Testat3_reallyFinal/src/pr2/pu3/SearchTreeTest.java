d1583fe96296b82f09bdf49f539c8c2a83f50d15
Steffen <Steffen@192.168.24.208>
2020-05-17 11:25:31 +0200

    final

--- PR2_Testat3_reallyFinal/src/pr2/pu3/SearchTreeTest.java
+++ PR2_Testat3_reallyFinal/src/pr2/pu3/SearchTreeTest.java
@@ -45,17 +45,17 @@ class SearchTreeTest {
 		var searchTree = new SearchTree<Integer>();
 
 		// searchTree contains no element
-		assertEquals(0, searchTree.height(searchTree.root));
+		assertEquals(0, searchTree.height());
 
 		// level 1
 		searchTree.add(100);
-		assertEquals(1, searchTree.height(searchTree.root));
+		assertEquals(1, searchTree.height());
 
 		// level 2, left childNode of 100
 		searchTree.add(50);
 		// level 2, right childNode of 100
 		searchTree.add(150);
-		assertEquals(2, searchTree.height(searchTree.root));
+		assertEquals(2, searchTree.height());
 
 		// level 3, left childNode of 50
 //		searchTree.add(25);
@@ -65,7 +65,7 @@ class SearchTreeTest {
 		searchTree.add(125);
 		// level 3, right childNode of 150
 //		searchTree.add(175);
-		assertEquals(3, searchTree.height(searchTree.root));
+		assertEquals(3, searchTree.height());
 
 		// level 4, left childNode of 25
 //		searchTree.add(20);
@@ -83,7 +83,7 @@ class SearchTreeTest {
 //		searchTree.add(170);
 		// level 4, right childNode of 175
 //		searchTree.add(180);
-		assertEquals(4, searchTree.height(searchTree.root));
+		assertEquals(4, searchTree.height());
 	}
 
 	/*
@@ -95,54 +95,54 @@ class SearchTreeTest {
 
 		// level 1
 		searchTree.add(100);
-		assertTrue(searchTree.contains(100, searchTree.root));
-		assertFalse(searchTree.contains(101, searchTree.root));
+		assertTrue(searchTree.contains(100));
+		assertFalse(searchTree.contains(101));
 
 		// level 2, left childNode of 100
-		assertFalse(searchTree.contains(50, searchTree.root));
+		assertFalse(searchTree.contains(50));
 		searchTree.add(50);
-		assertTrue(searchTree.contains(50, searchTree.root));
+		assertTrue(searchTree.contains(50));
 		// level 2, right childNode of 100
 		searchTree.add(150);
-		assertTrue(searchTree.contains(150, searchTree.root));
+		assertTrue(searchTree.contains(150));
 
 		// level 3, left childNode of 50
 		searchTree.add(25);
-		assertTrue(searchTree.contains(25, searchTree.root));
+		assertTrue(searchTree.contains(25));
 
 		// level 3, right childNode of 50
 		searchTree.add(75);
-		assertTrue(searchTree.contains(75, searchTree.root));
+		assertTrue(searchTree.contains(75));
 		// level 3, left childNode of 150
 		searchTree.add(125);
-		assertTrue(searchTree.contains(125, searchTree.root));
+		assertTrue(searchTree.contains(125));
 		// level 3, right childNode of 150
 		searchTree.add(175);
-		assertTrue(searchTree.contains(175, searchTree.root));
+		assertTrue(searchTree.contains(175));
 
 		// level 4, left childNode of 25
 		searchTree.add(20);
-		assertTrue(searchTree.contains(20, searchTree.root));
+		assertTrue(searchTree.contains(20));
 		// level 4, right childNode of 25
 		searchTree.add(30);
-		assertTrue(searchTree.contains(30, searchTree.root));
+		assertTrue(searchTree.contains(30));
 		// level 4, left childNode of 75
 		searchTree.add(70);
-		assertTrue(searchTree.contains(70, searchTree.root));
+		assertTrue(searchTree.contains(70));
 		// level 4, right childNode of 75
 		searchTree.add(80);
-		assertTrue(searchTree.contains(80, searchTree.root));
+		assertTrue(searchTree.contains(80));
 		// level 4, left childNode of 125
 		searchTree.add(120);
-		assertTrue(searchTree.contains(120, searchTree.root));
+		assertTrue(searchTree.contains(120));
 		// level 4, right childNode of 125
 		searchTree.add(130);
-		assertTrue(searchTree.contains(130, searchTree.root));
+		assertTrue(searchTree.contains(130));
 		// level 4, left childNode of 175
 		searchTree.add(170);
-		assertTrue(searchTree.contains(170, searchTree.root));
+		assertTrue(searchTree.contains(170));
 		// level 4, right childNode of 175
 		searchTree.add(180);
-		assertTrue(searchTree.contains(180, searchTree.root));
+		assertTrue(searchTree.contains(180));
 	}
 }
