3b4afd6accd2f40345d8d26a02a13a886e809582
Steffen <Steffen@192.168.24.208>
2020-06-07 09:31:37 +0200

    up

--- SteffenTestat4/src/de/hsmannheim/inf/pr2/ads/TimeCompare.java
+++ SteffenTestat4/src/de/hsmannheim/inf/pr2/ads/TimeCompare.java
@@ -6,68 +6,83 @@ import java.util.TreeSet;
 public class TimeCompare {
 
 	public static void main(String[] args) {
+		var tc = new TimeCompare();
 
-		var treeSet = new TreeSet<String>();
-		var hashSet = new HashSet<String>();
-
-		for (int i = 0; i < 10; i++)
-			System.out.println(CreateStrings.createStrings(8));
-
-//		System.out.println(getDurationTreeSetAdd(100));
-//		System.out.println(getDurationTreeSetAdd(1000));
-//		System.out.println(getDurationTreeSetAdd(10000));
-//		System.out.println(getDurationTreeSetAdd(100000));
-//		System.out.println(getDurationTreeSetAdd(1000000));
-//		System.out.println(getDurationHashSetAdd(100));
-//		System.out.println(getDurationHashSetAdd(1000));
-//		System.out.println(getDurationHashSetAdd(10000));
-//		System.out.println(getDurationHashSetAdd(100000));
-//		System.out.println(getDurationHashSetAdd(1000000));
-		System.out.println(getDurationTreeSetContains(100));
-		System.out.println(getDurationTreeSetContains(1000));
-		System.out.println(getDurationTreeSetContains(10000));
-		System.out.println(getDurationTreeSetContains(100000));
-		System.out.println(getDurationTreeSetContains(1000000));
+		System.out.println("TreeSet.add");
+		System.out.println(tc.getDurationTreeSetAdd(100));
+		System.out.println(tc.getDurationTreeSetAdd(1000));
+		System.out.println(tc.getDurationTreeSetAdd(10000));
+		System.out.println(tc.getDurationTreeSetAdd(100000));
+		System.out.println(tc.getDurationTreeSetAdd(1000000));
+		System.out.println("HashSet.add");
+		System.out.println(tc.getDurationHashSetAdd(100));
+		System.out.println(tc.getDurationHashSetAdd(1000));
+		System.out.println(tc.getDurationHashSetAdd(10000));
+		System.out.println(tc.getDurationHashSetAdd(100000));
+		System.out.println(tc.getDurationHashSetAdd(1000000));
+		System.out.println("TreeSet.contains");
+		System.out.println(tc.getDurationTreeSetContains(100));
+		System.out.println(tc.getDurationTreeSetContains(1000));
+		System.out.println(tc.getDurationTreeSetContains(10000));
+		System.out.println(tc.getDurationTreeSetContains(100000));
+		System.out.println(tc.getDurationTreeSetContains(1000000));
+		System.out.println("HashSet.contains");
+		System.out.println(tc.getDurationHashSetContains(100));
+		System.out.println(tc.getDurationHashSetContains(1000));
+		System.out.println(tc.getDurationHashSetContains(10000));
+		System.out.println(tc.getDurationHashSetContains(100000));
+		System.out.println(tc.getDurationHashSetContains(1000000));
 	}
 
-	static long getDurationTreeSetAdd(int n) {
-
-		var treeSet = new TreeSet<String>();
+	public long getDurationTreeSetAdd(int iterations) {
+		var treeSet = new TreeSet<>();
 		var start = System.currentTimeMillis();
-		for (int i = 0; i < n; i++)
+
+		for (var i = 0; i < iterations; i++)
 			treeSet.add(CreateStrings.createStrings(8));
-		return (System.currentTimeMillis() - start);
 
+		return (System.currentTimeMillis() - start);
 	}
 
-	static long getDurationHashSetAdd(int n) {
-
-		var hashSet = new HashSet<String>();
+	public long getDurationHashSetAdd(int iterations) {
+		var hashSet = new HashSet<>();
 		var start = System.currentTimeMillis();
-		for (int i = 0; i < n; i++)
+
+		for (var i = 0; i < iterations; i++)
 			hashSet.add(CreateStrings.createStrings(8));
-		return (System.currentTimeMillis() - start);
 
+		return (System.currentTimeMillis() - start);
 	}
 
-	static long getDurationTreeSetContains(int n) {
-
-		var treeSet = new TreeSet<String>();
-
-		for (int i = 0; i < 2 * n; i++)
+	public long getDurationTreeSetContains(int iterations) {
+		var treeSet = new TreeSet<>();
+		for (int i = 0; i < 2 * iterations; i++)
 			treeSet.add(CreateStrings.createStrings(8));
-		var toSearchFor = new String[n];
 
-		for (var a : toSearchFor) {
-			a = CreateStrings.createStrings(8);
-		}
-		System.out.println(toSearchFor.toString());
+		var toSearchFor = new String[iterations];
+		for (var i = 0; i < toSearchFor.length; i++)
+			toSearchFor[i] = CreateStrings.createStrings(8);
 
 		var start = System.currentTimeMillis();
-		for (int i = 0; i < n; i++)
-			treeSet.contains(CreateStrings.createStrings(8));
-		return (System.currentTimeMillis() - start);
+		for (int i = 0; i < iterations; i++)
+			treeSet.contains(toSearchFor[i]);
 
+		return (System.currentTimeMillis() - start);
 	}
 
+	public long getDurationHashSetContains(int iterations) {
+		var hashSet = new HashSet<>();
+		for (int i = 0; i < 2 * iterations; i++)
+			hashSet.add(CreateStrings.createStrings(8));
+
+		var toSearchFor = new String[iterations];
+		for (var i = 0; i < toSearchFor.length; i++)
+			toSearchFor[i] = CreateStrings.createStrings(8);
+
+		var start = System.currentTimeMillis();
+		for (int i = 0; i < iterations; i++)
+			hashSet.contains(toSearchFor[i]);
+
+		return (System.currentTimeMillis() - start);
+	}
 }
