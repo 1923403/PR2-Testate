ddc692ea3b56c6fa5eb4975e23bcd17f7de9f316
Steffen <Steffen@192.168.24.208>
2020-06-08 13:02:12 +0200

    up

--- SteffenTestat4/src/de/hsmannheim/inf/pr2/ads/CreateStrings.java
+++ SteffenTestat4/src/de/hsmannheim/inf/pr2/ads/CreateStrings.java
@@ -3,13 +3,18 @@ package de.hsmannheim.inf.pr2.ads;
 /**
  * AUFGABE 7<br>
  * <br>
- * The static method createStrings() of this class creates Strings of an
- * arbitrary length.
+ * This class creates Strings of a defined length.
  * 
  * @author Gruppe 25: Floris (1921233), Raphael (1926543), Steffen (1923403)
  */
 public class CreateStrings {
 
+	/**
+	 * Creates a String of a defined length.
+	 * 
+	 * @param length String's length
+	 * @return String
+	 */
 	public static String createStrings(int length) {
 		var word = "";
 
